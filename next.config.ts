6310852c1867c75bdcd317d15f2b21f507c5c1b9
CyberSphinxxx <johnlemargonzales@gmail.com>
2026-06-20 01:45:52 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "@radix-ui/react-dialog",
+      "@radix-ui/react-popover",
+      "@radix-ui/react-dropdown-menu",
+    ],
+  },
 };
 
 export default nextConfig;
